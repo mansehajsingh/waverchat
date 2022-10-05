ef6d1f7f4bc32ce093b630d4318c1b7c3929fd9c
Mansehaj Singh <msehajs@gmail.com>
2022-10-05 18:59:13 -0400

    Added and initialized UserCreationConfirmation model.

---
+++ api/src/main/java/com/waverchat/api/v1/User/UserConstants.java
@@ -0,0 +1,15 @@
+package com.waverchat.api.v1.User;
+
+public class UserConstants {
+
+    public static final int MIN_USERNAME_LENGTH = 4;
+    public static final int MAX_USERNAME_LENGTH = 20;
+
+    public static final int MIN_FIRST_NAME_LENGTH = 1;
+    public static final int MAX_FIRST_NAME_LENGTH = 35;
+
+    public static final int MAX_LAST_NAME_LENGTH = 35;
+
+    private UserConstants() {}
+
+}
