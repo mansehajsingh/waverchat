8b736bcc55ac7e88269c95e7e7c5bd05a95a172e
Mansehaj Singh <msehajs@gmail.com>
2022-10-05 22:51:14 -0400

    Created User entity.

---
+++ api/src/main/java/com/waverchat/api/v1/user/User.java
@@ -0,0 +1,53 @@
+package com.waverchat.api.v1.user;
+
+import com.waverchat.api.v1.ApplicationEntity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        }
+)
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends ApplicationEntity {
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    @Length(min = UserConstants.MIN_USERNAME_LENGTH, max = UserConstants.MAX_USERNAME_LENGTH)
+    private String username;
+
+    @NotNull
+    @NotEmpty
+    private String passwordHash;
+
+    @NotNull
+    @NotBlank
+    @Length(min = UserConstants.MIN_FIRST_NAME_LENGTH, max = UserConstants.MAX_FIRST_NAME_LENGTH)
+    private String firstName;
+
+    @NotNull
+    @Length(max = UserConstants.MAX_LAST_NAME_LENGTH)
+    private String lastName;
+
+    @NotNull
+    private boolean superUser = false;
+
+    @NotNull
+    private boolean deleted = false;
+
+}
