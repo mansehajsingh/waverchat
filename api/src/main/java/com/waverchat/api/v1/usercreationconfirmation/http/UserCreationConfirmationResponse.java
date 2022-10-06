62f94b2002920710c471ac461202b6a340571b1a
Mansehaj Singh <msehajs@gmail.com>
2022-10-05 23:58:29 -0400

    Added response body object for UserCreationConfirmation.

---
+++ api/src/main/java/com/waverchat/api/v1/usercreationconfirmation/http/UserCreationConfirmationResponse.java
@@ -0,0 +1,22 @@
+package com.waverchat.api.v1.usercreationconfirmation.http;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+public class UserCreationConfirmationResponse {
+
+    private UUID id;
+
+    private String email;
+
+    private String username;
+
+    private String firstName;
+
+    private String lastName;
+
+}
