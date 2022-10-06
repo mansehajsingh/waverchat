8d417a5ffe40d048323295af76228ffff8b18afe
Mansehaj Singh <msehajs@gmail.com>
2022-10-06 00:39:34 -0400

    Added password hashing to UserCreationConfirmationService service.

--- api/src/main/java/com/waverchat/api/v1/usercreationconfirmation/UserCreationConfirmationService.java
+++ api/src/main/java/com/waverchat/api/v1/usercreationconfirmation/UserCreationConfirmationService.java
@@ -3,6 +3,7 @@ package com.waverchat.api.v1.usercreationconfirmation;
 import com.waverchat.api.v1.exceptions.ConflictException;
 import com.waverchat.api.v1.exceptions.ResourceNotFoundException;
 import com.waverchat.api.v1.user.UserRepository;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,20 +25,25 @@ public class UserCreationConfirmationService {
         // if a user is already registered with the desired username we can't allow a new
         // user registry with that username
         if (userRepository.existsByUsername(userCreationConfirmation.getUsername())) {
-            throw new ConflictException("A registered user already exists with this username.");
+            throw new ConflictException("Username is already in use.");
         }
 
         // if a user is already registered with the desired email we cannot allow a new
         // account creation with the same email
         if (userRepository.existsByEmail(userCreationConfirmation.getEmail())) {
-            throw new ConflictException("A registered user already exists with this email.");
+            throw new ConflictException("A user already exists with this email.");
         }
 
         // if we have a user that has not confirmed their account yet with the desired username,
         // we cannot allow a new unconfirmed user creation with the same username
         if (userCreationConfirmationRepository.existsByUsername(userCreationConfirmation.getUsername())) {
-            throw new ConflictException("An unregistered user already exists with this username.");
+            throw new ConflictException("A user already exists with this username.");
         }
+
+        // hashing supplied user password before passing it on to the db
+        userCreationConfirmation.setPasswordHash(
+                BCrypt.hashpw(userCreationConfirmation.getPasswordHash(), BCrypt.gensalt())
+        );
 
         return userCreationConfirmationRepository.save(userCreationConfirmation);
     }
