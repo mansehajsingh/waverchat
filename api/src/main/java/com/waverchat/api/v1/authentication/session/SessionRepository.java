99c8053aa4bd8b6c3920ed14534a857e55566780
Mansehaj Singh <msehajs@gmail.com>
2022-10-08 16:14:50 -0400

    Created session repository.

---
+++ api/src/main/java/com/waverchat/api/v1/authentication/session/SessionRepository.java
@@ -0,0 +1,12 @@
+package com.waverchat.api.v1.authentication.session;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session, UUID> {
+    Optional<Session> findById(UUID id);
+}
